e20f9883e0e335d2ecfba68cf36c50390f23cc86
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-02-20 09:22:43 -0600

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::Path,
+    u128,
+};
+
+use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Employee {
+    department: String,
+    name: String,
+    age: u8,
+    salary: f64,
+}
+
+#[derive(Debug)]
+pub enum FileError {
+    NameIsEmpty,
+    InvalidExtension,
+    NotExists,
+}
+
+pub fn touch_default_file(filepath: &str) -> Result<()> {
+    let mut file = File::create(filepath)?;
+    file.write_all(b"department,name,age,salary\n")?;
+    Ok(())
+}
+
+pub fn check_file_exists(filepath: &str) -> Result<bool, FileError> {
+    let path = Path::new(&filepath);
+    match path.extension() {
+        None => {
+            if filepath.is_empty() {
+                return Err(FileError::NameIsEmpty);
+            } else {
+                return Err(FileError::InvalidExtension);
+            }
+        }
+        Some(_) => {
+            if path.exists() {
+                return Ok(true);
+            } else {
+                return Err(FileError::NotExists);
+            }
+        }
+    }
+}
+
+pub fn add_new(
+    filepath: &str,
+    department: String,
+    name: String,
+    age: u8,
+    salary: f64,
+) -> Result<()> {
+    let path = Path::new(&filepath);
+    let file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(path)?;
+    //let mut wtr = csv::Writer::from_writer(file);
+    //
+    let mut wtr = csv::WriterBuilder::new()
+        .has_headers(false)
+        .from_writer(file);
+
+    wtr.serialize(Employee {
+        department: department.to_string(),
+        name: name.to_string(),
+        age,
+        salary,
+    })?;
+
+    wtr.flush()?;
+    Ok(())
+}
+
+#[allow(unused_variables)]
+pub fn list_employees(filepath: &str, department: &str) -> Result<()> {
+    let path = Path::new(&filepath);
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_path(path)?;
+
+    let mut count: u128 = 0;
+    for result in rdr.deserialize() {
+        let empl: Employee = result?;
+        if empl.department == department {
+            print!("{:?}", empl);
+            count += 1;
+        }
+    }
+
+    if count == 0 {
+        return Err(anyhow!("No employees found in department {}.", department));
+    }
+    Ok(())
+}
+
+// Self explanatory
+pub fn check_if_departments_exist(filepath: &str) -> Result<bool> {
+    let path = std::path::Path::new(filepath);
+    if path.exists() {
+        Ok(true)
+    } else {
+        Err(anyhow!("Departments file does not exist."))
+    }
+}
+
+pub fn get_input_from_user() -> Result<String, String> {
+    unimplemented!()
+}
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use std::process;
+
+use clap::Parser;
+#[allow(unused_imports)]
+use department::{add_new, check_file_exists, list_employees, touch_default_file, FileError};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// file_path
+    #[arg(short, long)]
+    path: Option<String>,
+    /// List employees from department
+    #[arg(short, long, default_value_t = false)]
+    list: bool,
+    /// Add a new employee
+    #[arg(long, default_value_t = false)]
+    add: bool,
+    /// Department of the employee
+    #[arg(short, long)]
+    department: Option<String>,
+    /// Name of the employee
+    #[arg(short, long)]
+    name: Option<String>,
+    /// Age of the employee
+    #[arg(short, long)]
+    age: Option<u8>,
+    /// Salary of the employee
+    #[arg(short, long)]
+    salary: Option<f64>,
+}
+
+fn main() {
+    let args = Args::parse();
+    let filepath = match args.path {
+        Some(path) => path,
+        None => String::from("deps.csv"),
+    };
+
+    if !args.list && !args.add {
+        process::exit(0);
+    }
+
+    // Check that file exists
+    match check_file_exists(&filepath) {
+        Ok(_) => {}
+        Err(error) => match error {
+            FileError::NameIsEmpty => {
+                println!("Name of departments file cannot be empty.");
+                process::exit(1);
+            }
+            FileError::InvalidExtension => {
+                println!("Invalid file extension. Not a CSV File?");
+                process::exit(1);
+            }
+            // Create a new empty file if file doesn't exist
+            FileError::NotExists => {
+                println!(
+                    "Departments file {} does not exist. Creating an empty one...",
+                    filepath
+                );
+                match touch_default_file(&filepath) {
+                    Err(error) => {
+                        println!("Error creating new file: {error}");
+                        process::exit(1);
+                    }
+                    Ok(_) => {
+                        println!("Fille {} created succesfully.", filepath);
+                    }
+                }
+            }
+        },
+    }
+
+    if args.add {
+        let department = match args.department {
+            Some(dep) => &dep,
+            None => {
+                println!("Error: Department name is required.");
+                process::exit(1);
+            }
+        };
+        let name = match args.name {
+            Some(name) => &name,
+            None => {
+                println!("Error: Employee name is required.");
+                process::exit(1);
+            }
+        };
+        let age = match args.age {
+            Some(age) => age,
+            None => {
+                println!("Error: Age of employee is required.");
+                process::exit(1);
+            }
+        };
+        let salary = match args.salary {
+            Some(sal) => sal,
+            None => 0.0,
+        };
+
+        add_new(&filepath, department, name, age, salary).unwrap();
+        process::exit(0);
+
+        //add_new(
+        //    &filepath,
+        //    args.department.unwrap(),
+        //    args.name.unwrap(),
+        //    args.age.unwrap(),
+        //    args.salary.unwrap(),
+        //)
+        //.unwrap();
+    }
+
+    if args.list {
+        match args.department {
+            Some(dep) => list_employees(&filepath, &dep).unwrap(),
+            None => {
+                print!("Error: No department to filter with.");
+                process::exit(1);
+            }
+        }
+    }
+}
